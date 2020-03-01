c2defd63feea9fcba01306964d3962ead848bc3b
sunnyfeng <yfeng1997@gmail.com>
2020-03-01 18:31:45 -0500

    Add classes xml

--- app/src/main/java/com/sunnyfeng/rugraduating/AddClassesActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/AddClassesActivity.java
@@ -1,17 +1,78 @@
 package com.sunnyfeng.rugraduating;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class AddClassesActivity extends AppCompatActivity {
+public class AddClassesActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_classes);
-        Toolbar toolbar = findViewById(R.id.app_bar);
+
+        // Toolbar with title and options menu
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("RU Graduating");
+        }
+        toolbar.setSubtitle("Add your classes");
+        toolbar.inflateMenu(R.menu.options_menu);
+
+        // Drop down menu for majors
+        Spinner spinner = findViewById(R.id.major_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.major_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(this.getLocalClassName(), "Selected Item: " +item.getTitle());
+        // TODO: add intents to go to the activities/dialogs
+        switch (item.getItemId()) {
+            case R.id.add_program_item:
+                // do your code
+                return true;
+            case R.id.add_to_plan_item:
+                // do your code
+                return true;
+            case R.id.logout_item:
+                // do your code
+                return true;
+            case R.id.profile_item:
+                // do your code
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        String itemSelected = parent.getItemAtPosition(pos).toString();
+        // TODO: on item selected, filter courses list
+        Toast.makeText(this,itemSelected + " selected.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
