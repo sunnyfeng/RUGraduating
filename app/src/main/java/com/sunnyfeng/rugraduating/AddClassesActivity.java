d7df15057143644bb04adb4ea1a53b94a96b1210
sunnyfeng <yfeng1997@gmail.com>
2020-03-02 22:33:34 -0500

    adding more things

--- app/src/main/java/com/sunnyfeng/rugraduating/AddClassesActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/AddClassesActivity.java
@@ -1,5 +1,6 @@
 package com.sunnyfeng.rugraduating;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -30,19 +32,34 @@ public class AddClassesActivity extends AppCompatActivity implements AdapterView
         toolbar.inflateMenu(R.menu.options_menu);
 
         // Drop down menu for majors
-        Spinner spinner = findViewById(R.id.major_spinner);
+        Spinner spinner = findViewById(R.id.major_spinner_main);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.major_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        // TODO: implement RecyclerView with course list items
+
+        // Set up button
+        Button addClassesButton = findViewById(R.id.add_classes_button);
+        addClassesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO: add selected classes to DB
+                Intent intent = new Intent(AddClassesActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    // Inflate options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options_menu, menu);
         return true;
     }
+
+    // Add actions when option in menu is selected
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Log.d(this.getLocalClassName(), "Selected Item: " +item.getTitle());
@@ -58,21 +75,23 @@ public class AddClassesActivity extends AppCompatActivity implements AdapterView
                 // do your code
                 return true;
             case R.id.profile_item:
-                // do your code
+                Intent intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
+    // Handle major spinner item selected
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         String itemSelected = parent.getItemAtPosition(pos).toString();
         // TODO: on item selected, filter courses list
         Toast.makeText(this,itemSelected + " selected.", Toast.LENGTH_SHORT).show();
     }
 
+    // Handle major spinner item not selected
     public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
+        // TODO: maybe handle this? or just lead blank
     }
 }
