539a729f966a9778d94e5976e75fc1043bb0ec68
Nicholas Heah <44422670+nickheah07@users.noreply.github.com>
2020-03-20 17:24:58 -0400

    Login and sign up activities planned out

--- app/src/main/java/com/sunnyfeng/rugraduating/LoginActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.sunnyfeng.rugraduating;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +11,30 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //TODO: add splash page logo
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view){
+        EditText user_email = (EditText) findViewById(R.id.email_addr);
+        EditText user_pwd = (EditText) findViewById(R.id.pwd);
+
+        //TODO: check database if user exists
+        // if doesn't exist, return
+
+
+
+        // else, load intent, send login credentials?
+        Intent profile_page = new Intent(this, ProfileActivity.class);
+        startActivity(profile_page);
+
+
+    }
+
+    public void sign_up(View view){
+        Intent signup_page = new Intent(this, SignUpActivity.class);
+        startActivity(signup_page);
     }
 }
--- app/src/main/java/com/sunnyfeng/rugraduating/SignUpActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.sunnyfeng.rugraduating;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,26 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //TODO: populate spinners
+    }
+
+    public boolean signup_check_populated(){
+        boolean complete = true;
+        // TODO:check each field, highlight if un-entered, set complete to false
+
+        return complete;
+    }
+
+    public void create_user(View view){
+        // check all fields are populated
+        if(signup_check_populated()){
+            // TODO:create the user (add to database), new intent to add classes
+
+
+            Intent add_class_page = new Intent(this, AddClassesActivity.class);
+            startActivity(add_class_page);
+        }
+        // otherwise, do nothing. all unentered fields highlighted
     }
 }
