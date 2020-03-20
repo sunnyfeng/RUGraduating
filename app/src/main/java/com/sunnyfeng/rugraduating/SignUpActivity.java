d1f9727d976fb8992ee647268266f61cc6b5e0d6
Nicholas Heah <44422670+nickheah07@users.noreply.github.com>
2020-03-20 17:52:46 -0400

    populated some spinners in sign up

--- app/src/main/java/com/sunnyfeng/rugraduating/SignUpActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/SignUpActivity.java
@@ -3,8 +3,14 @@ package com.sunnyfeng.rugraduating;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -13,7 +19,31 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        //TODO: populate spinners
+        // populate spin_year
+        ArrayList<String> years = new ArrayList<String>();
+        int this_year = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = this_year - 3; i <= this_year + 4; i++) {
+            years.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> years_adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, years);
+
+        Spinner spin_year = (Spinner)findViewById(R.id.spin_year);
+        spin_year.setAdapter(years_adapter);
+        spin_year.setSelection(3);
+
+        // populate seasons
+        ArrayList<String> seasons = new ArrayList<String>();
+        seasons.add("Fall");
+        seasons.add("Winter");
+        seasons.add("Spring");
+        seasons.add("Summer");
+        ArrayAdapter<String> seasons_adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, seasons);
+
+        Spinner spin_season = (Spinner) findViewById(R.id.spin_season);
+        spin_season.setAdapter(seasons_adapter);
+        spin_season.setSelection(2);
+
+        //TODO: populate school and major spinners with database values
     }
 
     public boolean signup_check_populated(){
