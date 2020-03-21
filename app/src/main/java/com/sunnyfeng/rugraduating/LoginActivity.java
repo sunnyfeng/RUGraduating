d7ed12bf7f40a74adecdd28861876d724c0fc2f9
Nicholas Heah <44422670+nickheah07@users.noreply.github.com>
2020-03-20 22:42:47 -0400

    Checked required fields and fixed manifest for login and sign up

--- app/src/main/java/com/sunnyfeng/rugraduating/LoginActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/LoginActivity.java
@@ -17,23 +17,41 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    public boolean login_check_populated(){
+        boolean complete = true;
+
+        EditText email_textbox = (EditText) findViewById(R.id.email_addr);
+        String user_email = email_textbox.getText().toString();
+        if(user_email.length() == 0){
+            complete = false;
+            email_textbox.setError("Please enter your email");
+        }
+
+        EditText pwd_textbox = (EditText) findViewById(R.id.pwd);
+        String user_pwd = pwd_textbox.getText().toString();
+        if(user_pwd.length() == 0){
+            complete = false;
+            pwd_textbox.setError("Please enter your password");
+        }
+        return complete;
+    }
+
     public void login(View view){
-        EditText user_email = (EditText) findViewById(R.id.email_addr);
-        EditText user_pwd = (EditText) findViewById(R.id.pwd);
+        if(login_check_populated()) {
 
-        //TODO: check database if user exists
-        // if doesn't exist, return
-
+            //TODO: check database if user exists
+            // if doesn't exist, return
 
 
-        // else, load intent, send login credentials?
-        Intent profile_page = new Intent(this, ProfileActivity.class);
-        startActivity(profile_page);
+            // else, load intent, send login credentials?
+            Intent profile_page = new Intent(this, ProfileActivity.class);
+            startActivity(profile_page);
 
-
+        }
     }
 
     public void sign_up(View view){
+        // send straight to sign up page
         Intent signup_page = new Intent(this, SignUpActivity.class);
         startActivity(signup_page);
     }
--- app/src/main/java/com/sunnyfeng/rugraduating/SignUpActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/SignUpActivity.java
@@ -1,10 +1,13 @@
 package com.sunnyfeng.rugraduating;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +24,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         // populate spin_year
         ArrayList<String> years = new ArrayList<String>();
+        years.add("");
         int this_year = Calendar.getInstance().get(Calendar.YEAR);
         for (int i = this_year - 3; i <= this_year + 4; i++) {
             years.add(Integer.toString(i));
@@ -29,10 +33,11 @@ public class SignUpActivity extends AppCompatActivity {
 
         Spinner spin_year = (Spinner)findViewById(R.id.spin_year);
         spin_year.setAdapter(years_adapter);
-        spin_year.setSelection(3);
+        spin_year.setSelection(0);
 
         // populate seasons
         ArrayList<String> seasons = new ArrayList<String>();
+        seasons.add("");
         seasons.add("Fall");
         seasons.add("Winter");
         seasons.add("Spring");
@@ -41,14 +46,78 @@ public class SignUpActivity extends AppCompatActivity {
 
         Spinner spin_season = (Spinner) findViewById(R.id.spin_season);
         spin_season.setAdapter(seasons_adapter);
-        spin_season.setSelection(2);
+        spin_season.setSelection(0);
 
         //TODO: populate school and major spinners with database values
     }
 
     public boolean signup_check_populated(){
         boolean complete = true;
-        // TODO:check each field, highlight if un-entered, set complete to false
+
+        EditText name_textbox = (EditText)findViewById(R.id.name_sign_up);
+        String user_name = name_textbox.getText().toString();
+        if(user_name.length() == 0){
+            complete = false;
+            name_textbox.setError("Please enter your name");
+        }
+
+        Spinner seasons_spin = (Spinner)findViewById(R.id.spin_season);
+        String season = seasons_spin.getSelectedItem().toString();
+        if(season.length() == 0){
+            complete = false;
+
+            TextView errorText = (TextView)seasons_spin.getSelectedView();
+            errorText.setError("");
+            errorText.setTextColor(Color.RED);//just to highlight that this is an error
+
+        }
+
+        Spinner year_spin = (Spinner)findViewById(R.id.spin_year);
+        String year = year_spin.getSelectedItem().toString();
+        if(year.length() == 0){
+            complete = false;
+
+            TextView errorText = (TextView)year_spin.getSelectedView();
+            errorText.setError("");
+            errorText.setTextColor(Color.RED);//just to highlight that this is an error
+
+        }
+        /*
+        Spinner school_spin = (Spinner)findViewById(R.id.spin_school);
+        String school = school_spin.getSelectedItem().toString();
+        if(school.length() == 0){
+            complete = false;
+
+            TextView errorText = (TextView)school_spin.getSelectedView();
+            errorText.setError("");
+            errorText.setTextColor(Color.RED);//just to highlight that this is an error
+
+        }
+
+        Spinner major_spin = (Spinner)findViewById(R.id.spin_major);
+        String major = major_spin.getSelectedItem().toString();
+        if(major.length() == 0){
+            complete = false;
+
+            TextView errorText = (TextView)major_spin.getSelectedView();
+            errorText.setError("");
+            errorText.setTextColor(Color.RED);//just to highlight that this is an error
+
+        }
+        */
+        EditText email_textbox = (EditText)findViewById(R.id.email_sign_up);
+        String user_email = email_textbox.getText().toString();
+        if(user_email.length() == 0){
+            complete = false;
+            email_textbox.setError("Please enter your email");
+        }
+
+        EditText pwd_textbox = (EditText)findViewById(R.id.pwd_sign_up);
+        String user_pwd = pwd_textbox.getText().toString();
+        if(user_pwd.length() == 0){
+            complete = false;
+            pwd_textbox.setError("Please enter your password");
+        }
 
         return complete;
     }
