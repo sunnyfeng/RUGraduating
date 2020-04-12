1cc67d78a6175f7f05d6192b7d9f4a992ef3fdef
Avin Abraham <avin.abraham@rutgers.edu>
2020-04-12 13:03:14 -0400

    add more info to user class

--- app/src/main/java/com/sunnyfeng/rugraduating/LoginActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/LoginActivity.java
@@ -20,6 +20,8 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     GoogleSignInClient sClient = null;
     int RC_SIGN_IN = 72;
@@ -116,7 +118,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if(account != null){
             User mUser = ((User)getApplicationContext());
             mUser.setNetID(account.getEmail().split("@",2)[0]);
-
+            mUser.setEmail(account.getEmail());
+            String[] toCapitalize = account.getDisplayName().toLowerCase().split(" ");
+            mUser.setFirstName(String.valueOf(toCapitalize[0].charAt(0)).toUpperCase() + toCapitalize[0].substring(1));
+            mUser.setLastName(String.valueOf(toCapitalize[1].charAt(0)).toUpperCase() + toCapitalize[1].substring(1));
             //check here if user is in database to decide whether to sign-up or login
             //temporary just go to main page
             Intent main_page = new Intent(this, MainActivity.class);
--- app/src/main/java/com/sunnyfeng/rugraduating/User.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/User.java
@@ -5,6 +5,9 @@ import android.app.Application;
 public class User extends Application {
 
     private String netID = "not set";
+    private String firstName = "not set";
+    private String lastName = "not set";
+    private String email = "not set";
 
     public String getNetID() {
         return netID;
@@ -12,5 +15,29 @@ public class User extends Application {
 
     public void setNetID(String str) {
         netID = str;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String str) {
+        firstName = str;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String str) {
+        lastName = str;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String str) {
+        email = str;
     }
 }
