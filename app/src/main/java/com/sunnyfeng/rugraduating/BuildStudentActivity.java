3f09a7a73e16a575a5ab8cbb0b729c111df200c1
Avin Abraham <avin.abraham@rutgers.edu>
2020-04-20 22:42:26 -0400

    another one

--- app/src/main/java/com/sunnyfeng/rugraduating/BuildStudentActivity.java
+++ app/src/main/java/com/sunnyfeng/rugraduating/BuildStudentActivity.java
@@ -60,7 +60,7 @@ public class BuildStudentActivity extends AppCompatActivity {
                 });
 
         buildFulfilled.setRetryPolicy(new DefaultRetryPolicy(
-                10000,
+                20000,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
